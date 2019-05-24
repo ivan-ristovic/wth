552597d5bba67ce37a36cc7a853bf9acb3824156
Pljosan <ana.bajic13@gmail.com>
2019-05-24 15:41:37 +0200

    GTK

--- wth/app/Main.hs
+++ wth/app/Main.hs
@@ -1,30 +1,34 @@
 module Main where
 
--- totally not stolen
-
 import Lib
-import qualified Data.ByteString as BStr
-import Graphics.Gloss
-import Graphics.Gloss.Game
-import Graphics.Gloss.Juicy
 import Codec.Picture.Types
+import Control.Monad.Trans(liftIO)
+import qualified Data.ByteString as BStr
+import qualified Graphics.Gloss as G
+import qualified Graphics.Gloss.Game as GG
+import qualified Graphics.Gloss.Juicy as GJ
+import qualified Graphics.UI.Gtk as GTK
 
+-- window :: Display
+-- window = InWindow "wth" (400, 400) (10, 10)
 
-window :: Display
-window = InWindow "wth" (400, 400) (10, 10)
+-- background :: Color
+-- background = white
 
-background :: Color
-background = white
-
-drawing :: DynamicImage -> Float -> Picture
-drawing img _ = case fromDynamicImage img of 
-    Nothing  -> Blank
-    Just pngMap -> pictures [(png bgMapPath), pngMap]
+--drawing :: DynamicImage -> Float -> Picture
+--drawing img _ = case fromDynamicImage img of 
+--    Nothing  -> Blank
+--    Just pngMap -> pictures [(png bgMapPath), pngMap]
 
 main :: IO ()
 main = do
-    url <- formApiUrl "temp_new" 0 0 0
-    downloaded <- downloadMap url
-    case downloaded of
-        Left err  -> putStrLn err
-        Right img -> animate window background (drawing img)
+--    url <- formApiUrl "temp_new" 0 0 0
+--    downloaded <- downloadMap url
+--    case downloaded of
+--        Left err  -> putStrLn err
+--        Right img -> animate window background (drawing img)
+    GTK.initGUI
+    window <- GTK.windowNew
+    window `GTK.on` GTK.deleteEvent $ liftIO GTK.mainQuit >> return False
+    GTK.widgetShowAll window
+    GTK.mainGUI
